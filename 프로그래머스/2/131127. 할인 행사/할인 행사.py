d762512b6c1300008dfb461abff3c50e2e28a63d
김태훈 <twodf78@naver.com>
2024-04-04 22:11:18 +0900

    [level 2] Title: 할인 행사, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -0,0 +1,29 @@
+from functools import reduce
+def mapMaker(list):
+    map = {}
+    for e in list:
+        if e in map:
+            map[e] += 1
+        else:
+            map[e] = 1
+    return map
+
+def solution(want, number, discount):
+    answer, idx = 0, 0
+    total = reduce(lambda x, y: x+ y, number)
+    map = mapMaker(discount[idx: idx+total])
+    while idx + total <= len(discount):
+        for i in range(len(want)):
+            if(map.get(want[i]) != number[i]):
+                answer-=1
+                break
+        map[discount[idx]] = map.get(discount[idx]) - 1
+        idx += 1
+        answer += 1
+        if(idx + total > len(discount)):
+            break
+        if discount[idx + total - 1] in map:
+            map[discount[idx + total - 1]] = map.get(discount[idx + total - 1]) + 1
+        else :
+            map[discount[idx + total - 1]] = 1
+    return answer

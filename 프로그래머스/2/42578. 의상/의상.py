fe9a24df86670e1c0233e340b9f343f3b880f9a6
김태훈 <twodf78@naver.com>
2024-04-08 21:58:22 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,7 +1,6 @@
 from functools import reduce
 def solution(clothes):
-    answer = 0
     clothMap = {}
-    for [name, type] in clothes:
+    for [_, type] in clothes:
         clothMap[type] = clothMap.get(type) + 1 if type in clothMap else  1
     return reduce(lambda x,y: (x) * (y + 1) , clothMap.values(), 1) - 1 

f9efb49b0b42bd649465ba6c8e3f88e5e791bc8c
김태훈 <twodf78@naver.com>
2024-04-30 12:11:33 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,11 @@
+def solution(num):
+    answer = 0
+    for i in range(500):
+        if num == 1:
+            return answer
+        if num % 2 == 0:
+            num /= 2
+        else:
+            num = num * 3 + 1
+        answer+=1
+    return -1

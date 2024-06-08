e5356e0ee326e9270d1a988510623f712d3fc758
김태훈 <twodf78@naver.com>
2024-06-08 21:01:23 +0900

    [level 1] Title: 카드 뭉치, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,12 @@
+def solution(cards1, cards2, goal):
+    idx1, idx2 = 0,0 
+    for word in goal:
+        if idx1 < len(cards1) and cards1[idx1] == word:
+            idx1 += 1
+            continue
+        elif idx2 < len(cards2) and cards2[idx2] == word:
+            idx2 += 1
+            continue
+        else:
+            return "No"
+    return "Yes"

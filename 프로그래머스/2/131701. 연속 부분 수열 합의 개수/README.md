f35634c10c20649c8cfdf57b9cba94551a07a803
김태훈 <twodf78@naver.com>
2024-07-27 11:55:20 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 364.40 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/README.md
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 연속 부분 수열 합의 개수 - 131701 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701) 
 
 ### 성능 요약
 
-메모리: 43.8 MB, 시간: 321.37 ms
+메모리: 43.6 MB, 시간: 364.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 29일 21:37:31
+2024년 07월 27일 11:55:17
 
 ### 문제 설명
 
--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,8 +1,9 @@
 def solution(elements):
     current = list(elements)
-    answer = set(current)
-    for i in range(1, len(elements)):
-        for j in range(len(elements)):
+    answer = set()
+    for j in range(len(elements)):
+        answer.add(current[j])
+        for i in range(1, len(elements)):
             idx = (i + j) % len(elements)
             current[j] += elements[idx]
             answer.add(current[j])

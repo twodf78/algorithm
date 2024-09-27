c2d75e89057e5d36a3432ba7ea56f5cdd5abb8d4
김태훈 <twodf78@naver.com>
2024-09-28 00:03:13 +0900

    [level 2] Title: 전화번호 목록, Time: 279.78 ms, Memory: 184 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+class Solution {
+    public boolean solution(String[] phone_book) {
+        
+        HashSet<String> set = new HashSet();
+        List<String> phoneBook = new ArrayList<>(Arrays.asList(phone_book));
+        
+        Collections.sort(phoneBook, (a, b) -> a.length() - b.length());
+        
+        for(String phone: phoneBook){
+            for(int i = 1; i< phone.length(); i++){
+                if(set.contains(phone.substring(0, i))){
+                    return false;
+                }
+            }
+            set.add(phone);
+        }
+        return true;
+    }
+}

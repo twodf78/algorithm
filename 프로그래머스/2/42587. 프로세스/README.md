42b956d1b55c59bcee26e21bdb2d377a43d1d063
김태훈 <twodf78@naver.com>
2024-09-28 19:00:49 +0900

    [level 2] Title: 프로세스, Time: 1.04 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=java) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.12 ms
+메모리: 77.8 MB, 시간: 1.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 11일 14:42:20
+2024년 09월 28일 19:00:48
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int p : priorities){
+            pq.add(p);
+        }
+        while(!pq.isEmpty()){
+            for(int i = 0; i< priorities.length; i++){
+                if(priorities[i] == pq.peek()){
+                    pq.poll();
+                    answer++;
+                    if(i == location){
+                        return answer;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}

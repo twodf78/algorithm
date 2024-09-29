2d9b950b6a89574401a2f397e7a8997039d0d9ae
김태훈 <twodf78@naver.com>
2024-09-29 21:58:50 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 5.77 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -0,0 +1,75 @@
+import java.util.*;
+import java.util.stream.*;
+import java.io.*;
+class Solution {
+    int min = 100;
+    HashMap<Integer, List<Integer>> map = new HashMap<>();
+    public int solution(int n, int[][] wires) {
+        int answer = -1;
+        
+        for(int i=0; i< wires.length; i++){
+            Integer from = wires[i][0] - 1;
+            Integer to = wires[i][1] - 1;
+            List<Integer> fromList = map.get(from);
+            if(fromList == null){
+                fromList = new ArrayList<>();
+                fromList.add(to);
+                map.put(from, fromList);
+            }else{
+                fromList.add(to);
+            }
+            
+            List<Integer> toList = map.get(to);
+            if(toList == null){
+                toList = new ArrayList<>();
+                toList.add(from);
+                map.put(to, toList);
+            }else{
+                toList.add(from);
+            }
+        }
+        
+        for(int i=0; i< wires.length; i++){
+            calDif(i, n, wires);
+        }
+        
+        return min;
+    }
+    public void calDif(int idx, int n, int[][] wires){
+        int[] used = wires[idx];
+        
+        Queue<Integer> queue= new LinkedList<>();
+        queue.add(0);
+        
+        boolean[] visited = new boolean[n];
+        visited[0] = true;
+
+        dfs(used, queue, visited, wires);
+        
+        int size = 0;
+        for(int i = 0 ; i<visited.length; i++){
+            if(visited[i]){
+                size++;
+            }
+        }
+        min = Math.min(min,  Math.abs(size - (n - size)));
+    }
+    public void dfs(int[] used, Queue<Integer> queue, boolean[] visited, int[][] wires){
+        while(!queue.isEmpty()){
+            int from = queue.poll();
+            List<Integer> toList = map.get(from);
+            for(Integer to: toList){
+                if(visited[to]){
+                    continue;
+                }
+                if((used[0]-1 == from && used[1]-1 == to)
+                   || (used[0]-1 == to && used[1]-1 == from)){
+                    continue;
+                }
+                visited[to] = true;
+                queue.add(to);
+            }
+            dfs(used,queue, visited, wires);
+        }
+    }
+}

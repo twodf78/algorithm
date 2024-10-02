b7f44971f3962286be4d7ff72a502f297dea479f
김태훈 <twodf78@naver.com>
2024-10-02 22:28:19 +0900

    [level 3] Title: 숫자 게임, Time: 264.36 ms, Memory: 70.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.stream.*;
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        Integer[] integerA = Arrays.stream(A).boxed().toArray(Integer[]::new);
+        Integer[] integerB = Arrays.stream(B).boxed().toArray(Integer[]::new);
+        Arrays.sort(integerA, (a,b)->b-a);
+        Arrays.sort(integerB, (a,b)->b-a);
+        
+        int b = 0;
+        for(int a = 0; a<A.length; a++){
+            if(integerB[b] > integerA[a]){
+                b++;
+                answer++;
+            }
+        }
+        return answer;
+    }
+}

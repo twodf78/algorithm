4a9ba48b7b85d2de3459b9a426b4e0fbc4f3157f
김태훈 <twodf78@naver.com>
2024-10-05 00:08:23 +0900

    [level 3] Title: 이중우선순위큐, Time: 83.93 ms, Memory: 142 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,46 +2,25 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        int[] answer = new int[2]; // 결과를 담을 배열
-       
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        PriorityQueue<Integer> pqreverse = new PriorityQueue<>(Collections.reverseOrder());
+        int n = operations.length;
+        PriorityQueue<Integer> max = new PriorityQueue<>((a,b)->b-a);
+        PriorityQueue<Integer> min = new PriorityQueue<>();
         
-        int add = 0; // 추가된 숫자의 개수
-        int minus = 0; // 삭제된 숫자의 개수
-        
-        for (String operation : operations) {
-            String cmd = operation.split(" ")[0];
-            Integer num = Integer.parseInt(operation.split(" ")[1]);
-            
-            if (cmd.equals("I")) {
-                pq.add(num);
-                pqreverse.add(num);
-                add++;
-            } else {
-                minus++;
-                if (add <= minus) {
-                    pq.clear();
-                    pqreverse.clear();
-                    add = 0; // 초기화
-                    minus = 0; // 초기화
-                    continue;
-                }
-                if (num < 0) {
-                    int min = pq.poll();
-                    pqreverse.remove(min);
-                } else {
-                    int max = pqreverse.poll();
-                    pq.remove(max);
+        for(int i = 0; i< n; i++){
+            String[] op = operations[i].split(" ");
+            if(op[0].equals("I")){
+                max.add(Integer.parseInt(op[1]));
+                min.add(Integer.parseInt(op[1]));
+            }else{
+                if(Integer.parseInt(op[1]) > 0){
+                    Integer val = max.poll();
+                    min.remove(val);
+                }else{
+                    Integer val = min.poll();
+                    max.remove(val);
                 }
             }
         }
-
-        // 결과를 answer 배열에 할당
-        if (add > minus) {
-            answer[0] = pqreverse.poll(); // 최대값
-            answer[1] = pq.poll(); // 최소값
-        }
-        return answer;
+        return max.isEmpty() ? new int[]{0,0} : new int[]{max.peek(), min.peek()};
     }
 }

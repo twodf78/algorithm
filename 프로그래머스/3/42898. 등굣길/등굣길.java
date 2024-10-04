35802d5b38cd63e25c06e229536b5840eb78836f
김태훈 <twodf78@naver.com>
2024-10-05 01:20:59 +0900

    [level 3] Title: 등굣길, Time: 0.76 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,25 +1,25 @@
 import java.util.*;
 class Solution {
-    int[][] dp;
     public int solution(int m, int n, int[][] puddles) {
-        dp = new int[m+1][n+1];
-        
-        for(int i = 0; i<puddles.length;i++){
-            int[] p = puddles[i];
-            dp[p[0]][p[1]] = -1;
-        }
+        int[][] dp = new int[m+1][n+1];
         dp[m][n] = 1;
-        for(int i = m; i>0; i--){
-            for(int j = n ; j>0; j--){
-                if(dp[i][j] < 0){
+
+        for(int i = 0; i<puddles.length; i++){
+            dp[puddles[i][0]][puddles[i][1]] = -1;
+        }
+        
+        for(int i = m; i>=1 ; i--){
+            for(int j = n; j>=1 ; j--){
+                if(dp[i][j] == -1){
                     dp[i][j] = 0;
                     continue;
-                }if(i == m && j< n){
+                }
+                if(i == m && j < n){
                     dp[i][j] = dp[i][j+1];
                 }else if(i < m && j == n){
-                    dp[i][j] = dp[i + 1][j];
+                    dp[i][j] = dp[i+1][j];
                 }else if(i < m && j < n){
-                    dp[i][j] = (dp[i + 1][j] + dp[i][j+1]) % 1000000007;
+                    dp[i][j] = ( dp[i+1][j] + dp[i][j+1] ) % 1000000007;
                 }
             }
         }

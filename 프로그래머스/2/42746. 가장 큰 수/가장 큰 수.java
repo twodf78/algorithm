8d55992401fa22399446e9e929d1e017c7892ab6
김태훈 <twodf78@naver.com>
2024-10-05 00:28:08 +0900

    [level 2] Title: 가장 큰 수, Time: 2835.38 ms, Memory: 391 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -3,20 +3,15 @@ import java.util.stream.*;
 import java.io.*;
 class Solution {
     public String solution(int[] numbers) {
-        String answer = "";
-        String[] strNum = Arrays.stream(numbers).boxed().map(x->x.toString()).toArray(String[]::new);
-        List<String> list = new ArrayList<>(Arrays.asList(strNum));
+        String[] nums= Arrays.stream(numbers).boxed().map(x->x.toString()).toArray(String[]::new);
+        List<String> list = new ArrayList<>(Arrays.asList(nums));
+        Collections.sort(list, (a,b)->Integer.parseInt(b+a) - Integer.parseInt(a+b));
         
-        Collections.sort(list, (a, b)->{
-            Integer aInt = Integer.parseInt(a + b);
-            Integer bInt = Integer.parseInt(b + a);
-            return bInt - aInt;        
-        });
-        
-        String num= list.stream().reduce("", (acc, x) -> acc+ x);
-        if(num.substring(0, 1).equals("0")){
-            return "0";
+        if(list.get(0).equals("0")) return "0";
+        String ans = "";
+        for(String str: list){
+            ans+=str;
         }
-        return num;
+        return ans;
     }
 }

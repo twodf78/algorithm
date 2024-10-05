cf08cce4ab6eaf33f56acf649840ad22cf1cf868
김태훈 <twodf78@naver.com>
2024-10-05 20:08:56 +0900

    [level 3] Title: N으로 표현, Time: 54.73 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -4,14 +4,13 @@ import java.io.*;
 class Solution {
     public int solution(int N, int number) {
         List<HashSet<Integer>> list = new ArrayList<>();
-        list.add(new HashSet<>());
         
         for(int i = 1; i<=8; i++){
             HashSet<Integer> set = new HashSet<>();
             set.add(Integer.parseInt(String.valueOf(N).repeat(i)));
-            for(int j = 1; j< i; j++){
+            for(int j = 0; j< i - 1; j++){
                 HashSet<Integer> jSet = list.get(j);
-                HashSet<Integer> kSet = list.get(i - j);
+                HashSet<Integer> kSet = list.get(i - 1 - j - 1);
                 for(Integer jVal : jSet){
                     for(Integer kVal: kSet){
                         set.add(jVal + kVal);
--- 프로그래머스/3/42895. N으로 표현/README.md
+++ 프로그래머스/3/42895. N으로 표현/README.md
@@ -1,10 +1,10 @@
 # [level 3] N으로 표현 - 42895 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895#) 
 
 ### 성능 요약
 
-메모리: 88.6 MB, 시간: 34.63 ms
+메모리: 76.5 MB, 시간: 54.73 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 05일 19:57:13
+2024년 10월 05일 20:08:53
 
 ### 문제 설명
 

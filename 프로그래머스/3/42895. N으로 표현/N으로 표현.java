257c79847a659f7e9833de342f2b4fb45899b1fc
김태훈 <twodf78@naver.com>
2024-10-05 19:57:16 +0900

    [level 3] Title: N으로 표현, Time: 34.63 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -2,32 +2,30 @@ import java.util.*;
 import java.util.stream.*;
 import java.io.*;
 class Solution {
-    List<HashSet<Integer>> setList = new ArrayList<>();
     public int solution(int N, int number) {
-        setList.add(new HashSet<>());
-        for(int cnt = 1; cnt <= 8; cnt++){
+        List<HashSet<Integer>> list = new ArrayList<>();
+        list.add(new HashSet<>());
+        
+        for(int i = 1; i<=8; i++){
             HashSet<Integer> set = new HashSet<>();
-            set.add(Integer.parseInt(String.valueOf(N).repeat(cnt)));
-            for(int i = 1; i< cnt; i++){
-                HashSet<Integer> iSet = setList.get(i);
-                HashSet<Integer> jSet = setList.get(cnt - i);
-                    for(Integer in: iSet){
-                        for(Integer jn: jSet){
-                            set.add(in + jn);
-                            set.add(in - jn);
-                            set.add(in * jn);
-                            if(jn != 0){
-                                set.add(in / jn);
-                            }
+            set.add(Integer.parseInt(String.valueOf(N).repeat(i)));
+            for(int j = 1; j< i; j++){
+                HashSet<Integer> jSet = list.get(j);
+                HashSet<Integer> kSet = list.get(i - j);
+                for(Integer jVal : jSet){
+                    for(Integer kVal: kSet){
+                        set.add(jVal + kVal);
+                        set.add(jVal - kVal);
+                        set.add(jVal * kVal);
+                        if(kVal!=0){
+                            set.add(jVal / kVal);
                         }
                     }
+                }
             }
-            if(set.contains(number)){
-                return cnt;
-            }
-            setList.add(set);
+            if(set.contains(number)) return i;
+            list.add(set);
         }
-        
         return -1;
     }
 }

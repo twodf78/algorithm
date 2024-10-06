a9050fa9eb6b4e987cd6d09aadeb503f51f050ab
김태훈 <twodf78@naver.com>
2024-10-06 15:06:18 +0900

    [level 1] Title: K번째수, Time: 3.50 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,18 +4,18 @@ import java.io.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        Integer[] integerArray = Arrays.stream(array).boxed().toArray(Integer[]::new);
-
-        List<Integer> list = new ArrayList<>(Arrays.asList(integerArray));
-        for(int idx = 0; idx< commands.length; idx++){
-            int[] cmd = commands[idx];
-            int i = cmd[0];
-            int j = cmd[1];
-            int k = cmd[2];
-            List<Integer> part = new ArrayList<>(list.subList(i - 1, j)); // 부분 리스트 추출
-            Collections.sort(part);
-            answer[idx] = part.get(k - 1);
+        Integer[] array_ = Arrays.stream(array).boxed().toArray(Integer[]::new);
+        List<Integer> arrayList = new ArrayList<>(Arrays.asList(array_));
+        
+        
+        for(int idx = 0; idx<commands.length; idx++){
+            int i = commands[idx][0] - 1;
+            int j = commands[idx][1];
+            int k = commands[idx][2] - 1;
             
+            List<Integer> part = new ArrayList<>(arrayList.subList(i, j));
+            Collections.sort(part);
+            answer[idx] = part.get(k);
         }
         return answer;
     }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
 
 ### 성능 요약
 
-메모리: 79 MB, 시간: 2.99 ms
+메모리: 72.5 MB, 시간: 3.50 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 28일 22:55:04
+2024년 10월 06일 15:06:15
 
 ### 문제 설명
 

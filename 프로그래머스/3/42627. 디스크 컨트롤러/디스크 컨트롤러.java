c819c092ecf78c217cb281bd6385d33f17a00d8d
김태훈 <twodf78@naver.com>
2024-10-06 14:51:55 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 21.64 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,78 +1,49 @@
 import java.util.*;
 import java.io.*;
 import java.util.stream.*;
-class Job{
-    public int start;
-    public int realStart;
-    public int need;
-    
-    Job(int start, int realStart, int need){
-        this.start= start;
-        this.realStart= realStart;
-        this.need= need;
-    }
-    @Override
-    public String toString(){
-        return "start:" + start + " realStart:" + realStart + " need:" + need;
-    }
-}
 class Solution {
     public int solution(int[][] jobs) {
-        int answer = 0;
-        
-        PriorityQueue<Job> pq = new PriorityQueue<>((a, b)->a.need-b.need);
-        HashMap<Integer, List<Job>> map = new HashMap<>();
-        Integer max = Collections.max(Arrays.asList(jobs).stream().map((x)->x[1]).collect(Collectors.toList()));
+        // 0 - 도착 시간, 1-소요시간 (->실제종료시간)
+        PriorityQueue<int[]> ready = new PriorityQueue<>((a, b) -> a[1] - b[1]);
 
-        for(int[] job: jobs){
-            Job jobObject = new Job(job[0], -1, job[1]);
-            List<Job> jobList = map.getOrDefault(job[0], new ArrayList<>());
-            jobList.add(jobObject);
-            map.put(job[0],jobList);
+        
+        // 0 - 도착 시간, 1-소요시간
+        List<int[]> jobList = new ArrayList<>();
+        for(int i = 0; i<jobs.length; i++){
+            int[] val = jobs[i];
+            jobList.add(val);
         }
         
-        Job currentJob = null;
-        for(int i = 0; i<=max* jobs.length;i++){
-            
-            //아무것도 없을 때
-            if(pq.isEmpty() && currentJob == null){
-                List<Job> jobList = map.get(i);
-                if(jobList != null){
-                    for(Job job: jobList){
-                        pq.add(job);
-                    }
-                    pq.peek().realStart = i;
-                }
-                continue;
-            }
-            
-            if(currentJob==null){
-                currentJob = pq.poll();
-            }
-            boolean done = false;
-            
-            //작업 끝내기
-            if(currentJob.realStart + currentJob.need == i){
-                answer += i - currentJob.start;
-                done = true;
-                currentJob = null;
-            }
-            
-            
-            List<Job> jobList = map.get(i);
-            //이번에 들어오는 게 있을때
-            if(jobList != null){
-                for(Job job: jobList){
-                    pq.add(job);
-                }
-            }
-            
-            //현재 자리 차지하는게 없을 때, 바꿔주기
-            if(done && !pq.isEmpty()){
-                pq.peek().realStart = i;
-            }
-        }
+        Collections.sort(jobList, (a, b)->a[0] - b[0]);
         
-        return (int)Math.floor(answer/jobs.length);
+        int pIdx = 0;
+        int answer = 0;
+        int[] current = null;
+        for(int time = jobList.get(0)[0]; time < jobs.length * 1000; time++){
+            
+            while(pIdx< jobList.size() && jobList.get(pIdx)[0] <= time){
+                ready.add(new int[]{jobList.get(pIdx)[0], jobList.get(pIdx)[1]});
+                pIdx++;
+            }
+            if(current ==null && !ready.isEmpty()){
+                current = ready.poll();
+                current[1]+=time;
+            }
+            //현재거 종료
+            if(current!=null && current[1] <= time){
+                answer += current[1] - current[0];
+                
+                if(!ready.isEmpty()){
+                    current = ready.poll();
+                    current[1]+=time;
+                }else{
+                    current = null;
+                }
+                
+                
+            }
+            if(ready.isEmpty() && pIdx >= jobList.size() && current == null) break;
+        }
+        return answer/jobs.length;
     }
 }

73b71cc150ead9c8da03e24b915cfeea31168a7b
김태훈 <twodf78@naver.com>
2024-10-06 14:16:39 +0900

    [level 2] Title: 프로세스, Time: 1.80 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
 
 ### 성능 요약
 
-메모리: 77.8 MB, 시간: 1.04 ms
+메모리: 76.5 MB, 시간: 1.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 28일 19:00:48
+2024년 10월 06일 14:16:37
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,23 +2,26 @@ import java.util.*;
 import java.io.*;
 class Solution {
     public int solution(int[] priorities, int location) {
-        int answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        
-        for(int p : priorities){
-            pq.add(p);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            return b[0] - a[0];
+        });
+        Queue<int[]> queue = new LinkedList<>(); 
+        for(int i = 0; i< priorities.length ; i++){
+            pq.add(new int[] {priorities[i], i});
+            queue.add(new int[] {priorities[i], i});
         }
-        while(!pq.isEmpty()){
-            for(int i = 0; i< priorities.length; i++){
-                if(priorities[i] == pq.peek()){
-                    pq.poll();
-                    answer++;
-                    if(i == location){
-                        return answer;
-                    }
-                }
+        
+        int count = 0;
+        while(!queue.isEmpty()){
+            int[] first = queue.poll();
+            if(first[0] == pq.peek()[0]){
+                pq.poll();
+                count++;
+                if(first[1] == location) return count;
+            }else{
+                queue.add(first);
             }
         }
-        return answer;
+        return count;
     }
 }

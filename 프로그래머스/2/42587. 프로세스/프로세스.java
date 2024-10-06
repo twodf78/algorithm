7e3499f49bb1b30db4c46cc0b95f52e3899376f0
김태훈 <twodf78@naver.com>
2024-10-06 14:18:03 +0900

    [level 2] Title: 프로세스, Time: 9.60 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,19 +2,17 @@ import java.util.*;
 import java.io.*;
 class Solution {
     public int solution(int[] priorities, int location) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
-            return b[0] - a[0];
-        });
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) ->  b - a);
         Queue<int[]> queue = new LinkedList<>(); 
         for(int i = 0; i< priorities.length ; i++){
-            pq.add(new int[] {priorities[i], i});
+            pq.add(priorities[i]);
             queue.add(new int[] {priorities[i], i});
         }
         
         int count = 0;
         while(!queue.isEmpty()){
             int[] first = queue.poll();
-            if(first[0] == pq.peek()[0]){
+            if(first[0] == pq.peek()){
                 pq.poll();
                 count++;
                 if(first[1] == location) return count;

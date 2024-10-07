7f36c044c55c80d800ec2a53eacdada39cc92f9a
김태훈 <twodf78@naver.com>
2024-10-07 20:54:03 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 85.16 ms, Memory: 95.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 95 MB, 시간: 82.82 ms
+메모리: 95.2 MB, 시간: 85.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 27일 23:32:04
+2024년 10월 07일 20:53:58
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,29 +2,25 @@ import java.util.*;
 import java.io.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i = 0; i< participant.length; i++){
+            Integer count = map.getOrDefault(participant[i], 0);
+            map.put(participant[i], count + 1);
+        }
+        
+        for(int i = 0; i< completion.length; i++){
+            Integer count = map.get(completion[i]);
+            if(count == 1){
+                map.remove(completion[i]);
+                continue;
+            }
+            map.put(completion[i], count - 1);
+        }
+        
         String answer = "";
-        HashMap<String, Integer> completeMap = new HashMap<>();
-        for(String p : participant){
-            Integer num = completeMap.get(p);
-            if(num == null){
-                completeMap.put(p, 1);
-            }else{
-                completeMap.put(p, num + 1);
-            }
+        for(String ans : map.keySet()){
+            answer = ans;
         }
-        for(String p : completion){
-            Integer num = completeMap.get(p);
-            if(num-1 == 0){
-                completeMap.remove(p);
-            }else{
-                completeMap.put(p, num - 1);
-            }
-        }
-        
-        for(Map.Entry<String, Integer> entry: completeMap.entrySet()){
-            answer = entry.getKey();
-        }
-        
         return answer;
     }
 }

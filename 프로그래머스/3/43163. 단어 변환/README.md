c947df673fbbfd3d58ee30dcada0aef3febb9065
김태훈 <twodf78@naver.com>
2024-10-07 21:37:05 +0900

    [level 3] Title: 단어 변환, Time: 0.69 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
 
 ### 성능 요약
 
-메모리: 88.4 MB, 시간: 1.17 ms
+메모리: 78.8 MB, 시간: 0.69 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 30일 23:44:09
+2024년 10월 07일 21:37:02
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,79 +1,72 @@
 import java.util.*; 
 class Solution {
+    String t;
     int answer = Integer.MAX_VALUE;
-    String targetFinal;
-    int n ;
-    int [][] dp;
+    int[][] map; 
+    int n;
     public int solution(String begin, String target, String[] words) {
+        t = target;
         n = words.length;
-        targetFinal = target;
-        dp = new int[n][n];
+        map = new int[n][n + 1];
         
-        for(int i = 0; i<n; i++){
-            String a = words[i];
-            for(int j = i + 1; j<n; j++){
-                String b = words[j];
-                if(isConvertable(a,b)){
-                    dp[i][j] = 1;
-                    dp[j][i] = 1;
+        for(int i = 0; i< n; i++){
+            String wi = words[i];
+            for(int j = i + 1; j< n; j++){
+                String wj= words[j];
+                if(isConvertable(wi, wj)){
+                    map[i][j] = 1; 
+                    map[j][i] = 1; 
                 }
             }
-            
+            if(isConvertable(wi, target)){
+                map[i][n] = 1;
+            }
         }
-
         
-        for(int i = 0; i<n; i++){
-            if(isConvertable(begin,words[i])){
+        for(int i = 0; i< n; i++){
+            String wi = words[i];
+            if(isConvertable(wi, begin)){
+                Queue<Integer> queue = new LinkedList<>();
+                queue.add(i);
+
                 boolean[] visit = new boolean[n];
-                Queue<Integer> idxQueue = new LinkedList<>();
-                idxQueue.add(i);
-                visit[i] = true; 
-                bfs(idxQueue, visit, words);
+                visit[i] = true;
+                bfs(queue, visit, words);
             }
         }
         
         return answer == Integer.MAX_VALUE ? 0 : answer;
     }
-    public void bfs(Queue<Integer> idxQueue, boolean[] visit, String[] words){
-        int cnt = 1;
-        while(!idxQueue.isEmpty()){
-            Queue<Integer> newQueue = new LinkedList<>();
-            while(!idxQueue.isEmpty()){
-                Integer idx = idxQueue.poll();
-                
-                if(words[idx].equals(targetFinal)){
-                    answer = Math.min(answer, cnt);
-                    break;
-                }
-                for(int i = 0; i<n;i++){
-                    if(i == idx || visit[i] || dp[i][idx]!=1){
-                        continue;
-                    }
-                    newQueue.add(i);
-                    visit[i] = true; 
-                }
-            }
-            if(newQueue.isEmpty()){
-                break;
-            }
-            idxQueue = newQueue;
-            cnt++;
-        }
-        return;
-    }
-    
-    public boolean isConvertable(String a, String b){
+    public void bfs(Queue<Integer> queue, boolean[] visit, String[] words) {
         int count = 0;
-        for(int i = 0; i< a.length(); i++){
-            String ca= a.substring(i, i+1);
-            String cb= b.substring(i, i+1);
-            if(!ca.equals(cb)){
-                count++;
+        while(!queue.isEmpty()){
+            count++;
+            Queue<Integer> newQueue = new LinkedList<>();
+            while(!queue.isEmpty()){
+                Integer idx = queue.poll();
+                if(words[idx].equals(t)){
+                    answer = Math.min(answer, count);
+                }
+
+                int[] nextList = map[idx];
+                for(int i = 0; i< n;i++){
+                    int next = nextList[i];
+                    if(next == 0 || visit[i]) continue;
+                    visit[i] = true;
+                    newQueue.add(i);
+                }
             }
-            if(count >= 2){
-                return false;
+            if(!newQueue.isEmpty()){
+                queue = newQueue;
             }
         }
-        return count == 1;
+    }
+    public boolean isConvertable(String from, String to) {
+        int count = 0;
+        for(int i = 0; i< from.length();i++){
+            if(from.charAt(i) != to.charAt(i)) count++;
+            if(count >= 2) return false;
+        }
+        return count <= 1;
     }
 }

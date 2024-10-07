517566051a4b87bbf5e0b9bd440fd9046184e084
김태훈 <twodf78@naver.com>
2024-10-07 22:08:57 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 15.53 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전력망을 둘로 나누기 - 86971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971?language=java#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971) 
 
 ### 성능 요약
 
-메모리: 83.7 MB, 시간: 127.17 ms
+메모리: 80.4 MB, 시간: 15.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 29일 22:01:44
+2024년 10월 07일 22:08:54
 
 ### 문제 설명
 
--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,81 +1,47 @@
 import java.util.*;
 import java.util.stream.*;
-import java.io.*;
 class Solution {
     int min = 100;
-    HashMap<Integer, List<Integer>> map = new HashMap<>();
+    int[][] map;
     public int solution(int n, int[][] wires) {
-        int answer = -1;
-        
-        for(int i=0; i< wires.length; i++){
-            Integer from = wires[i][0] - 1;
-            Integer to = wires[i][1] - 1;
-            List<Integer> fromList = map.get(from);
-            if(fromList == null){
-                fromList = new ArrayList<>();
-                fromList.add(to);
-                map.put(from, fromList);
-            }else{
-                fromList.add(to);
-            }
-            
-            List<Integer> toList = map.get(to);
-            if(toList == null){
-                toList = new ArrayList<>();
-                toList.add(from);
-                map.put(to, toList);
-            }else{
-                toList.add(from);
-            }
+        map = new int[n+1][n+1];
+        for(int i = 0; i< wires.length; i++){
+            int v1 = wires[i][0];
+            int v2 = wires[i][1];
+            map[v1][v2] = 1;
+            map[v2][v1] = 1;
         }
         
-        for(int i=0; i< wires.length; i++){
-            calDif(i, n, wires);
+        for(int i = 0; i< wires.length;i++){
+            Queue<Integer> queue = new LinkedList<>();
+            queue.add(1);
+            boolean[] visit = new boolean[n+1];
+            visit[1] = true;
+            bfs(queue, visit, wires[i]);
         }
-        
         return min;
     }
-    public void calDif(int idx, int n, int[][] wires){
-        int[] used = wires[idx];
-        
-        Queue<Integer> queue= new LinkedList<>();
-        queue.add(0);
-        
-        boolean[] visited = new boolean[n];
-        visited[0] = true;
-
-        dfs(used, queue, visited, wires);
-        
-        int size = 0;
-        System.out.print(used[0] - 1+"  ");
-        System.out.print(used[1] - 1+"  ");
-            System.out.println();
-        for(int i = 0 ; i<visited.length; i++){
-            
-            System.out.println(size);
-            System.out.print(visited[i]+" ");
-            if(visited[i]){
-                size++;
-            }
-        }
-        min = Math.min(min,  Math.abs(size - (n - size)));
-    }
-    public void dfs(int[] used, Queue<Integer> queue, boolean[] visited, int[][] wires){
+    
+    public void bfs(Queue<Integer> queue, boolean[] visit, int[] cut) {
         while(!queue.isEmpty()){
-            int from = queue.poll();
-            List<Integer> toList = map.get(from);
-            for(Integer to: toList){
-                if(visited[to]){
+            Integer v = queue.poll();
+            int[] nextList = map[v];
+            for(int i = 0; i< nextList.length;i++){
+                int connected = nextList[i];
+                if(visit[i] || connected == 0) continue;
+                if( (v == cut[0] && i == cut[1])
+                  || (v == cut[1] && i == cut[0])){
                     continue;
                 }
-                if((used[0]-1 == from && used[1]-1 == to)
-                   || (used[0]-1 == to && used[1]-1 == from)){
-                    continue;
-                }
-                visited[to] = true;
-                queue.add(to);
+                visit[i] = true;
+                queue.add(i);
             }
-            dfs(used,queue, visited, wires);
         }
+        int visited = 0;
+        for(int i = 0 ; i< visit.length; i++){
+            if(visit[i]) visited++;
+        }
+        int dif = Math.abs((visit.length - 1 - visited) - visited);
+        min = Math.min(min, dif);
     }
 }

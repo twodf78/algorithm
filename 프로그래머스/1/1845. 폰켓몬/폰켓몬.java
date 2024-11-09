ebee677e4c1d071038380d851a5d1b427475a45c
김태훈 <twodf78@naver.com>
2024-11-09 20:54:00 +0900

    [level 1] Title: 폰켓몬, Time: 11.29 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,12 +1,11 @@
-import java.util.HashSet;
-import java.util.Arrays;
-
+import java.util.*;
 class Solution {
     public int solution(int[] nums) {
-        HashSet<Integer> set=new HashSet<>();
-        for(int i = 0; i<nums.length;i++){
+        HashSet<Integer> set = new HashSet<>();
+        int n = nums.length;
+        for(int i = 0 ; i<n ; i++){
             set.add(nums[i]);
         }
-        return Math.min(set.size(), nums.length / 2);
+        return Math.min(set.size(), n / 2);
     }
 }

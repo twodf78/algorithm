e32a4663572835ab2e801b99761af5f87d5a05e1
김태훈 <twodf78@naver.com>
2024-11-09 21:19:10 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,21 +1,16 @@
 import java.util.*;
-import java.io.*;
 class Solution {
     public int solution(String[][] clothes) {
         HashMap<String, Integer> map = new HashMap<>();
-        
-        for(int i = 0 ; i< clothes.length;i++){
-            String name = clothes[i][0];
-            String type = clothes[i][1];
-            
-            Integer count = map.getOrDefault(type, 0);
-            map.put(type, count + 1);
+        for(int i = 0; i< clothes.length; i++){
+            String[] clothesTuple = clothes[i];
+            Integer count = map.getOrDefault(clothesTuple[1], 0);
+            map.put(clothesTuple[1], count + 1);
         }
-        int answer =1;
-        for(Integer count: map.values()){
-            answer*= count + 1;
+        int answer = 1;
+        for(Integer val : map.values()){
+            answer *= (val+1);
         }
         return answer - 1;
-
     }
 }
